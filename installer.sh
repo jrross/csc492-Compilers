e487272b4293b3da4f14f5fe00fb20214105907e
7275982 <jeffrey.ross@mines.sdsmt.edu>
2019-01-28 20:55:56 -0700

    Compilers

---
+++ installer.sh
@@ -0,0 +1,13 @@
+ #!/bin/sh
+
+make
+
+sudo ./pyinstaller --onefile testingMain.py progInfo.py ParserV2Parser.py ParserV2Listener.py ParserV2Lexer.py operations.py
+
+make clean
+
+cp ./dist/testingMain $PWD
+
+sudo chown $USER -R ./build ./dist
+
+rm -rf ./build ./dist
